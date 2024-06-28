74a559576798e38930361ea564bc1a09f3b5284b
da2045 <vitde2009@gmail.com>
2024-06-29 00:58:19 +0300

    Update dz4.py

--- dz4.py
+++ dz4.py
@@ -1,20 +1,37 @@
-#№3
+№1
 # num1 = int(input("enter your fitst num -->"))
 # num2 = int(input("enter your second num -->"))
 # for i in range(num1, num2, 1):
 #     if i % 7 == 0:
 #         print(i)
 
+#№2
+# num1 = int(input("enter your fitst num -->"))
+# num2 = int(input("enter your second num -->"))
+# print ("1:")
+# for i in range(num1, num2, 1):
+#      print (i)
+# print ("2:")
+# for i in range(num2, num1, -1):
+#      print (i)
+# print ("3:")
+# for i in range(num1, num2, 1):
+#      if i % 7 == 0:
+#          print(i)
+# print ("4:")
+# for i in range(num1, num2, 1):
+#      if i % 5 == 0:
+#           print (i)
+
+№3
 num1 = int(input("enter your fitst num -->"))
 num2 = int(input("enter your second num -->"))
-print ("1:")
 for i in range(num1, num2, 1):
-     print (i)
-print ("2:")
-for i in range(num2, num1, -1):
-     print (i)
-print ("3:")
+    if i % 3 == 0:
+        print("fizz")
 for i in range(num1, num2, 1):
-     if i % 7 == 0:
-         print(i)
-
+    if i % 5 == 0:
+        print ("buzz")
+for i in range(num1, num2, 1):
+    if i % 5 == 0 {i} % 3 == 0:
+        print ("fizz buzz")
